91fbfd0cb1f3ecc1c5803c8eee2cd704dac10070
geoafrikana <geoafrikana@gmail.com>
2023-01-02 19:01:34 +0100

    drf api

--- buildings/views.py
+++ buildings/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Building
+from .serializers import BuildingSerializer
 
-# Create your views here.
+class BuildingViewSet(viewsets.ModelViewSet):
+    queryset = Building.objects.all()
+    serializer_class = BuildingSerializer
